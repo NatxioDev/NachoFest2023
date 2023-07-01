40ad9a27d7e74515deb037d046bbf990b8a26f7c
Ignacio Paz <natxio.dev@gmail.com>
2023-07-01 02:41:32 -0400

    change: principal structure

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,23 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 
 import "./index.css";
-// import Header from "./components/Header.jsx";
+import Header from "./components/Header.jsx";
 import Countdown from "./components/Countdown.jsx";
+import Maps from "./components/Maps.jsx";
+import Footer from "./components/Footer.jsx";
+import Asistance from "./components/Asistance";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <Header />
       <Countdown />
+      <br />
+      <Maps />
+      <br />
+      <br />
+      <Asistance />
+      <Footer />
     </ChakraProvider>
   </React.StrictMode>
 );
