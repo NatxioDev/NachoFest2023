c58ba92021a131c4dab238c9bd8e78d99d5a4016
Ignacio Paz <natxio.dev@gmail.com>
2023-07-01 02:40:56 -0400

    add: asistance section

---
+++ src/components/Asistance.jsx
@@ -0,0 +1,22 @@
+import { Button, Text, Center } from "@chakra-ui/react";
+
+import { FaWhatsapp } from "react-icons/fa";
+
+function Asistance() {
+  return (
+    <Center>
+      <Text color={"white"}>Confirma tu asistencia:</Text>
+      <Button
+        as={"a"}
+        href="https://api.whatsapp.com/send?phone=59169766811&text=Confirmo%20asistencia%20%3A)"
+        colorScheme="whatsapp"
+        leftIcon={<FaWhatsapp />}
+        marginLeft={"10px"}
+      >
+        Whatsapp
+      </Button>
+    </Center>
+  );
+}
+
+export default Asistance;
